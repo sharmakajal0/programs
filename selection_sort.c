7c76940157b5197dfc67f7dda853b662eabddf06
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-06-12 13:48:44 +0530

    added selection_sort in c

--- selection_sort.c
+++ selection_sort.c
@@ -8,6 +8,17 @@ void swap(int *a, int *b){
 	*b = temp;
 }
 
+void print(int i){
+	if(i == 1)
+		printf("%dst sort\n", i);
+	else if(i == 2)
+		printf("%dnd sort\n", i);	
+	else if(i == 3)
+		printf("%drd sort\n", i);
+	else
+		printf("%dth sort\n", i);
+}
+
 void printArray(int arr[], int n){
 	for(int i=0;i<n;i++){
 		printf("%d ", arr[i]);
@@ -15,37 +26,39 @@ void printArray(int arr[], int n){
 	printf("\n");
 }
 
-void selection_sort(int arr[], int n){
-	int min_index;
-	int j;
-	for(int i=0;i<=n-1;i++){
-		min_index = i;
-		j = i + 1;
-		for(;j<n;j++){
-			if(arr[j] > arr[min_index]){
-				min_index = j;
-			}
-			swap(&arr[min_index], &arr[j]);
-		}
-	}
-}
-
-
 int main(){
 	int arr[MAX_SIZE];
-		
-	printf("Enter elements of Array");
-
 	for(int i=0;i<MAX_SIZE;i++){
 		scanf("%d", &arr[i]);
 	}
 	
-	printf("Befor sort");
+	printf("Before Sort: \n");
 	printArray(arr, MAX_SIZE);
 
-	selection_sort(arr, MAX_SIZE);
+	printf("\n");
+	int i=0, j;
+	int min;
+	int count = 0;
+	for(;i<MAX_SIZE-1;i++){
+		min = i;
+		j = i+1;
+		while(j < MAX_SIZE){
+			if(arr[j] < arr[min]){
+				min = j;
+			}
+			j++;
+		}
+		if(min != i){
+			count += 1;
+			swap(&arr[min], &arr[i]);
+			//print(count);
+			//printArray(arr, MAX_SIZE);
+			//printf("\n");
 
-	printf("After Sort");
+		}
+	}
+
+	printf("After Sort: \n");
 	printArray(arr, MAX_SIZE);
 
 	return 0;
