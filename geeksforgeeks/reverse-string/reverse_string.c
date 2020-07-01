ba11db95c4c10280527ffc4e22f535f20b8797ea
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2020-07-01 21:42:01 +0530

    added program to reverse the given string

--- geeksforgeeks/reverse-string/reverse_string.c
+++ geeksforgeeks/reverse-string/reverse_string.c
@@ -2,10 +2,7 @@
 #include<string.h>
 
 #define MAX 1000
-
-void inputString(char *s){
-    gets(s);
-}
+// #pragma GCC diagnostic ignored ""
 
 int size(char *s){
     int length = 0;
@@ -41,18 +38,35 @@ void reverse_string(char *s, int size){
 }
 
 int main(){
+    FILE *stdin;
     int T;
-    scanf("%d", &T);
     // while(T != 0){
-    char string[20][20];
+    
+    char string[20][MAX];
+    stdin = fopen("input/input.in", "r");
+    
+    scanf("%d", &T);
+    if(stdin == NULL){
+        perror("Error opening file");
+        return -1;
+    }
 
     for(int i=0;i<T;i++){
-        gets(string[i]);
-        int length = size(string[i]);
-
-        reverse_string(string[i], length);
-        puts(string[i]);
+        fgets(string[i], MAX, stdin);
     }
+
+    for(int i=0;i<T;i++){
+        int length = size(string[i]);
+        if(length == 0){
+            printf("Given string is empty.\n");
+        }
+        else if(length != 0){
+            reverse_string(string[i], length);
+            printString(string[i]);
+        }
+    }
+
+    fclose(stdin);
     
     return 0;
 }
