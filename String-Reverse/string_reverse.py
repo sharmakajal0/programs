4e303aaa1b80d1769e1f4f1c768c175912ec5a72
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2022-06-01 22:59:30 +0530

    Modified the string reverse program to work recursively

--- String-Reverse/string_reverse.py
+++ String-Reverse/string_reverse.py
@@ -1,8 +1,13 @@
 #!usr/env/python3
 
 def reverseString(teststr):
-    string = teststr[::-1]
-    return string
+    if len(teststr) == 0:
+        return
+    
+    temp = teststr[0]
+    reverseString(teststr[1:])
+    print(temp, end='')
 
 testString = "ChandanSharma"
-print(reverseString(testString))
+reverseString(testString)
+print()
